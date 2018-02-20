0af4083c0a9ffc49891425acbe8a2f46d868db1d
Bailey Parker <b@ileyparker.com>
2018-02-20 02:30:59 -0500

    Make lints report all issues (Fix #5)

--- tests/simple_test/test_lint_fixtures.py
+++ tests/simple_test/test_lint_fixtures.py
@@ -1,3 +1,4 @@
+import re
 from unittest import main, TestCase
 
 from simple_test.fixtures import discover_fixtures
@@ -5,15 +6,22 @@ from simple_test.fixtures import discover_fixtures
 
 class TestLintFixtures(TestCase):
     def test_fixtures_are_snake_case(self):
+        non_snake_case = []
+
         for fixture in discover_fixtures():
             for part in fixture.relative_sim_file_path.with_suffix('').parts:
-                self.assertRegex(part, r'^[a-z0-9]+(_[a-z0-9]+)*$',
-                                 msg="fixture files should be snake case: {}"
-                                 .format(fixture.relative_sim_file_path))
+                if not re.match(r'^[a-z]+(_[a-z0-9]+)*$', part):
+                    non_snake_case.append(fixture.relative_sim_file_path)
+
+        if non_snake_case:
+            self.fail("Fixture names must be snake case:\n\n{}"
+                      .format('\n'.join("\t{}".format(n)
+                                        for n in non_snake_case)))
 
     def test_no_duplicate_sim_files(self):
         sim_file_contents = {}
         sim_files = set()
+        duplicates = []
 
         for fixture in discover_fixtures():
             if fixture.sim_file_path not in sim_files:
@@ -21,13 +29,17 @@ class TestLintFixtures(TestCase):
                     contents = f.read()
 
                     if contents in sim_file_contents:
-                        self.fail("identical sim files {} and {}"
-                                  .format(fixture.relative_sim_file_path,
-                                          sim_file_contents[contents]))
+                        duplicates.append((fixture.relative_sim_file_path,
+                                           sim_file_contents[contents]))
 
                     sim_file_contents[contents] = \
                         fixture.relative_sim_file_path
                     sim_files.add(fixture.sim_file_path)
+
+        if duplicates:
+            self.fail("Identical sim files:\n\n{}\n\nPlease merge them!"
+                      .format('\n'.join("\t{} and {}".format(a, b)
+                                        for a, b in duplicates)))
 
 
 if __name__ == '__main__':
